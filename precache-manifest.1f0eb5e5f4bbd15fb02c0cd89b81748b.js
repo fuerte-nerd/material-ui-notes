75f9adc86e8872d1a3a1695f3d7fcba4173dbc52
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-19 16:01:50 +0100

    Updates

---
+++ precache-manifest.1f0eb5e5f4bbd15fb02c0cd89b81748b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b487daa86e2d4641778586f751f60914",
+    "url": "/material-ui-notes/index.html"
+  },
+  {
+    "revision": "6edc6dab1146978d071a",
+    "url": "/material-ui-notes/static/css/main.20513167.chunk.css"
+  },
+  {
+    "revision": "32161a39c528544bdda4",
+    "url": "/material-ui-notes/static/js/2.cbe5041b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-ui-notes/static/js/2.cbe5041b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6edc6dab1146978d071a",
+    "url": "/material-ui-notes/static/js/main.e4988af4.chunk.js"
+  },
+  {
+    "revision": "78d3b8d87b8521c9f4ef",
+    "url": "/material-ui-notes/static/js/runtime-main.1ab6f809.js"
+  }
+]);
